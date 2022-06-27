86f0b302bb3d1005ba54d76f4c98d9b38d47473b
TollyH <ptolemyhill@sky.com>
2022-06-27 19:38:57 +0100

    Implement HashPassword function

--- SummerPortfolioProject/Pages/PageBase.cs
+++ SummerPortfolioProject/Pages/PageBase.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SummerPortfolioProject.Pages
 {
@@ -12,12 +14,33 @@ namespace SummerPortfolioProject.Pages
             LoggedInUsername = HttpContext.Session.GetString("LoggedInUsername");
         }
 
+        /// <summary>
+        /// Redirect the user to the forbidden error page if they aren't logged in.
+        /// </summary>
         public void RequireLoggedIn()
         {
             if (LoggedInUsername is null)
             {
                 Response.Redirect("/Error/Forbidden");
             }
+        }
+
+        /// <summary>
+        /// Generate a salted SHA512 hash for a password.
+        /// </summary>
+        /// <param name="password">The plaintext password to hash</param>
+        /// <param name="salt">The optional salt to use. If null a random one will be generated.</param>
+        /// <returns>The resulting salted hash in the format "{hash}:{salt}"</returns>
+        public string HashPassword(string password, string? salt = null)
+        {
+            if (salt is null)
+            {
+                byte[] saltBytes = RandomNumberGenerator.GetBytes(32);
+                salt = Convert.ToBase64String(saltBytes);
+            }
+            SHA512 sha512 = SHA512.Create();
+            byte[] computedHash = sha512.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
+            return $"{Convert.ToHexString(computedHash)}:{salt}";
         }
     }
 }
