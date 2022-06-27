cbc2e8ac6d5f2fef609804f585f35f1d695abd27
TollyH <ptolemyhill@sky.com>
2022-06-27 16:34:56 +0100

    Add missing pages

---
+++ SummerPortfolioProject/Pages/ModerateCandidates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SummerPortfolioProject.Pages
+{
+    public class ModerateCandidatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SummerPortfolioProject/Pages/ModeratePortfolios.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SummerPortfolioProject.Pages
+{
+    public class ModeratePortfoliosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
