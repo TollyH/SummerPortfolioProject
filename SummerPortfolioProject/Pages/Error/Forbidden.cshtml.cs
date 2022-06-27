6b0e343040e5bb988130387840142e3906adf0c6
TollyH <ptolemyhill@sky.com>
2022-06-27 17:36:35 +0100

    Basic login checks

---
+++ SummerPortfolioProject/Pages/Error/Forbidden.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SummerPortfolioProject.Pages.Error
+{
+    public class ForbiddenModel : PageBase
+    {
+    }
+}
