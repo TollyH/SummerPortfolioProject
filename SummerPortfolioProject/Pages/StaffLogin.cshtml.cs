72bce2fe9c3755475181d4d2cca045df35faf441
TollyH <ptolemyhill@sky.com>
2022-06-28 20:06:27 +0100

    Created login form and functionality

--- SummerPortfolioProject/Pages/StaffLogin.cshtml.cs
+++ SummerPortfolioProject/Pages/StaffLogin.cshtml.cs
@@ -6,5 +6,23 @@ namespace SummerPortfolioProject.Pages
     public class StaffLoginModel : PageBase
     {
         public StaffLoginModel(MySqlConnector.MySqlConnection sqlConnection) : base(sqlConnection) { }
+
+        internal bool showAuthError = false;
+
+        public void OnPost()
+        {
+            string? username = HttpContext.Request.Form["Username"];
+            string? password = HttpContext.Request.Form["Password"];
+
+            if (username is not null && password is not null)
+            {
+                if (Authenticate(username, password))
+                {
+                    HttpContext.Session.SetString("LoggedInUsername", username);
+                    Response.Redirect("/?newauth=1");
+                }
+            }
+            showAuthError = true;
+        }
     }
 }
