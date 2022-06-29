67aee2a4c9a2f36fc1400470cf87f746607f881f
TollyH <ptolemyhill@sky.com>
2022-06-29 13:55:21 +0100

    Simplify staff login OnPost

--- SummerPortfolioProject/Pages/StaffLogin.cshtml.cs
+++ SummerPortfolioProject/Pages/StaffLogin.cshtml.cs
@@ -11,16 +11,13 @@ namespace SummerPortfolioProject.Pages
 
         public void OnPost()
         {
-            string? username = HttpContext.Request.Form["Username"];
-            string? password = HttpContext.Request.Form["Password"];
+            string username = HttpContext.Request.Form["Username"];
+            string password = HttpContext.Request.Form["Password"];
 
-            if (username is not null && password is not null)
+            if (Authenticate(username, password))
             {
-                if (Authenticate(username, password))
-                {
-                    HttpContext.Session.SetString("LoggedInUsername", username);
-                    Response.Redirect("/?newauth=1");
-                }
+                HttpContext.Session.SetString("LoggedInUsername", username);
+                Response.Redirect("/?newauth=1");
             }
             showAuthError = true;
         }
